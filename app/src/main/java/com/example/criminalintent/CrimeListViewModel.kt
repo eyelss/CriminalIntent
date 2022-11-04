15ef316a9d6e63c0308b579d0a0b699212f12f33
eyelss <stud0000200014@study.utmn.ru>
2022-11-04 22:29:58 +0500

    finish chapter

--- app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -3,16 +3,6 @@ package com.example.criminalintent
 import androidx.lifecycle.ViewModel
 
 class CrimeListViewModel : ViewModel() {
-    val crimes = mutableListOf<Crime>()
-
-    init {
-        for (i in 0 until 100) {
-            val crime = Crime()
-            crime.title = "Crime #$i"
-            crime.isSolved = i % 2 == 0
-            // if (i == 13)
-            //    crime.requiresPolice = true
-            crimes += crime
-        }
-    }
+    private val crimeRepository = CrimeRepository.get()
+    val crimeListLivedata = crimeRepository.getCrimes()
 }
